0c2cb8c270939de8b178911e86c572ce04592836
Dima <dimapanteleyvrach@gmail.com>
2023-06-13 01:36:43 +0300

    дз

---
+++ src/main/java/com/example/test3java/Test3javaApplication.java
@@ -0,0 +1,13 @@
+package com.example.test3java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test3javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test3javaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test3java/Test3javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test3java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test3javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
